629150e00d240159374bb7cb6bd610c2ba672725
Minhyuk Kim <kimminhyuk1004@gmail.com>
2020-07-25 22:07:57 +0900

    Initial

---
+++ CPPStart/CPPStart/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  CPPStart
+//
+//  Created by Minhyuk Kim on 2020/07/25.
+//  Copyright © 2020 Mininny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    using namespace std;
+    
+    cout << "Hello, World!" << endl;
+    return 0;
+}
