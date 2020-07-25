6517a6926a1ca7c995c8757397d3cad01b33f815
Jaesung <chic0815@konkuk.ac.kr>
2020-07-26 02:21:10 +0900

    Update main.cpp
    😉

--- CPPStart/CPPStart/main.cpp
+++ CPPStart/CPPStart/main.cpp
@@ -21,23 +21,23 @@ int main(int argc, const char * argv[]) {
     /* Multi-line
      comment */
     
+    numberOfCarrots();
+    printRand();
+    convertCelsiusToFarenheit();
+    
+    return 0;
+}
+
+void numberOfCarrots() {
     int carrots;
+    double x;
     
     cout << "당근 몇개가 있나요?" << endl;
     cin >> carrots;
     cout << "당근 " << carrots << "개를 흔들어 주세요!" << endl;
     
-    double x = pow(carrots, 2);
+    x = pow(carrots, 2);
     cout << "제곱은 " << x << "개" << endl;
-    
-    printRand();
-    
-    cout << "섭씨 온도를 입력하세요: ";
-    double celcius;
-    cin >> celcius;
-    cout << "화씨론: " << celciusToFarenheit(celcius) << endl;
-    
-    return 0;
 }
 
 void printRand() {
@@ -46,6 +46,13 @@ void printRand() {
     return;
 }
 
-double celciusToFarenheit(double celcius) {
+void convertCelsiusToFarenheit() {
+    double celcius;
+    cout << "섭씨 온도를 입력하세요: ";
+    cin >> celcius;
+    cout << "화씨론: " << farenheitFrom(celcius) << endl;
+}
+
+double farenheitFrom(double celcius) {
     return celcius * 1.8 + 32.0;
 }
